8976bc1c16f3a6f09c8a6b5d7326a6983c79fab3
VinnyG-mus <viniciusrocha337@gmail.com>
2025-07-28 14:50:47 -0300

    Delete Projeto teoria musical directory

--- Projeto teoria musical/busca.h
+++
@@ -1,8 +0,0 @@
-#ifndef BUSCA_H
-#define BUSCA_H
-
-#include "escala.h"
-
-Escala* buscaBinariaRecursiva(Escala arr[], int inicio, int fim, char* nome);
-
-#endif
--- Projeto teoria musical/ordenacao.h
+++
@@ -1,9 +0,0 @@
-#ifndef ORDENACAO_H
-#define ORDENACAO_H
-
-#include "escala.h"
-
-void merge(Escala arr[], int l, int m, int r);
-void mergeSort(Escala arr[], int l, int r);
-
-#endif
